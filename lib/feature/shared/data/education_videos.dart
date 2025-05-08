80cc5ff09edbc9afcec953d4d6a27fa3acce25ed
김도영 <binj1246@gmail.com>
2025-05-08 11:54:13 +0900

    feat :: add data

---
+++ lib/feature/shared/data/education_videos.dart
@@ -0,0 +1,39 @@
+import '../../model/video.dart';
+
+List<Video> educationVideos = [
+  Video(
+      videoId: 'y6U-VRHwXXs',
+      title: '반려동물과의 소통',
+      description: '이번 강좌에선 반려동물과 어떻게 대화할지,  혼자서 알아서하는 반려동물이 있을지 알려 드립니다!'
+  ),
+  Video(
+      videoId: 'i-zNBLSgV1s',
+      title: '펫티켓 & 동물보호법',
+      description: '이번 강좌에선 반려인들이 필히 알고 있어야할 펫티켓 및 법적 사항을 알려 드립니다!'
+  ),
+  Video(
+      videoId: 'bWoCN-ER3UM',
+      title: '반려동물의 음식',
+      description: '이번 강좌에선 사료만으로는 부족한 부분을 채워 줄 음식들에 대해 알려 드립니다!'
+  ),
+  Video(
+      videoId: '2g4bod1v_TQ' ,
+      title: '반려동물 산책',
+      description: '이번 강좌에선 반려견과 올바르게 산책하는 방법을 알려 드립니다!'
+  ),
+  Video(
+      videoId: '8kENAN-g5Mw' ,
+      title: '반려동물의 장난감',
+      description: "이번 강좌에선 장난감 놀이에 대한 이해 및 '풍부화교육'의 개념에 대해 알려 드립니다!"
+  ),
+  Video(
+      videoId: 'hS_7mb3uaac' ,
+      title: '반려동물의 간식',
+      description: "이번 강좌에선 간식을 줄 때 알아두면 좋은 사항들을 알려 드립니다1"
+  ),
+  Video(
+      videoId: 'KnJa8sTi7k8' ,
+      title: '반려동물에 대해',
+      description: "이번 강좌에선 반려견과 보호자의 좋은 관계를 위한 방법이 무엇인지 알려 드립니다!"
+  ),
+];
